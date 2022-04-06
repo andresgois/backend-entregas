5481030e3c8ab04ced7a1c985d1fffa327e4cc1b
Andre gois <andre.s.gois3@gmail.com>
2022-04-06 20:13:24 -0300

    :sparkles: feat: create controller client

---
+++ src/modules/clients/useCases/CreateClientController.ts
@@ -0,0 +1,16 @@
+import { Response, Request } from "express";
+import { CreateClientUseCase } from "./createClientUseCase";
+
+export class CreateClientController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const createClientUseCase = new CreateClientUseCase();
+    const result = await createClientUseCase.execute({
+      username,
+      password,
+    });
+
+    return response.json(result);
+  }
+}
